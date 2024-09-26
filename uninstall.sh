81dbced966fb915b60673e0cd8acf104462475a3
TheMurmabis <30555036+TheMurmabis@users.noreply.github.com>
2024-09-26 16:09:44 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,16 @@
-#Остановка и отключиение сервиса
+#Остановка и отключение сервиса
+echo "Stopping and disabling the service"
 sudo systemctl stop myapp
 sudo systemctl disable myapp
 
 #Удаление systemd unit файла
+echo "Deleting the systemd unit file"
 sudo rm /etc/systemd/system/myapp.service
 
-#Перезагрузапусе systemd
+#Перезапуск systemd
+echo "Restarting systemd"
 sudo systemctl daemon-reload
 
 #Удаление директории с проектом
+echo "Deleting a directory with a project"
 sudo rm -rf /root/web
