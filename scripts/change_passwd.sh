be8a304735a9fff5dc10c59ab1fe7269a94279ee
TheMurmabis <ildarmurtazin@ro.ru>
2024-12-01 14:40:41 +0300

    FIx change_passwd.sh

--- scripts/change_passwd.sh
+++ scripts/change_passwd.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Activating virtual environment..."
-source venv/bin/activate
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
+echo "Activating virtual environment..."
+source "$SCRIPT_DIR/../venv/bin/activate"
 
 echo "You are about to change the admin password. Please make sure you remember the new password."
 read -p "Do you want to proceed? (Y/N): " confirm
 
-
 if [[ "$confirm" =~ ^[Yy]$ ]]; then
-    ADMIN_PASS=$(python3 -c "from main import change_admin_password; change_admin_password()")
+    ADMIN_PASS=$(PYTHONPATH="$SCRIPT_DIR/.." python3 -c "from main import change_admin_password; change_admin_password()")
     echo -e "Admin password successfully changed to: \e[32m$ADMIN_PASS\e[0m"
 else
     echo "Password change canceled."
