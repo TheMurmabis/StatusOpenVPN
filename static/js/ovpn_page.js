d280001a9a8217d500cdaf1633de6ff2346b9d3f
TheMurmabis <ildarmurtazin@ro.ru>
2025-01-18 19:45:47 +0300

    Update ovpn.html

--- static/js/ovpn_page.js
+++ static/js/ovpn_page.js
@@ -61,6 +61,37 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    // Получаем состояние галочки из localStorage
+    var checkbox = document.getElementById("hide_undef");
+    var hideUndefState = localStorage.getItem("hide_undef_state");
+
+    // Устанавливаем состояние галочки в зависимости от сохраненного значения
+    checkbox.checked = hideUndefState === "true";
+
+
     // Вызов функции для преобразования времени
     convertToUserTimezone();
+
+    // Включаем фильтрацию в зависимости от состояния галочки
+    toggleUNDEF();
 });
+
+// Функция для фильтрации строк в зависимости от состояния чекбокса
+function toggleUNDEF() {
+    var checkbox = document.getElementById("hide_undef");
+    var rows = document.querySelectorAll(".log-row");
+
+    // Сохраняем состояние галочки в localStorage
+    localStorage.setItem("hide_undef_state", checkbox.checked);
+
+    rows.forEach(function (row) {
+        var clientName = row.querySelector("td:first-child").textContent.trim(); // Получаем текст из первого столбца (Клиент)
+
+        // Если в столбце "Клиент" значение "UNDEF" и чекбокс установлен, скрываем строку
+        if (checkbox.checked && clientName === "UNDEF") {
+            row.style.display = "none";
+        } else {
+            row.style.display = ""; // Показываем строку, если условие не выполнено
+        }
+    });
+}
