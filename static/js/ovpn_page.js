e7c8f4f550fd9282dd51f73df0af53bf0b36bcc4
TheMurmabis <ildarmurtazin@ro.ru>
2026-03-15 23:29:57 +0300

    Fix

--- static/js/ovpn_page.js
+++ static/js/ovpn_page.js
@@ -1,20 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Кэширование элементов
-    const showIPCheckbox = document.getElementById("show-ip-checkbox");
     const showColumnsCheckbox = document.getElementById("show-columns-checkbox");
     const speedColumns = document.querySelectorAll(".speed-column");
-    const savedShowIPState = localStorage.getItem("showRealIP");
     const savedShowColumnsState = localStorage.getItem("showColumns");
 
-    // Восстановление состояния реальных IP
-    if (savedShowIPState === "true") {
-        showIPCheckbox.checked = true;
-        toggleRealIP(true);
-    } else {
-        showIPCheckbox.checked = false;
-        toggleRealIP(false);
-    }
-    // Восстановление состояния столбцов
     if (savedShowColumnsState === "true") {
         showColumnsCheckbox.checked = true;
         toggleColumns(true);
@@ -23,31 +11,15 @@ document.addEventListener("DOMContentLoaded", () => {
         toggleColumns(false);
     }
 
-    // Обработчики событий
-    showIPCheckbox.addEventListener("change", () => {
-        toggleRealIP(showIPCheckbox.checked);
-        localStorage.setItem("showRealIP", showIPCheckbox.checked);
-    });
-
     showColumnsCheckbox.addEventListener("change", () => {
         toggleColumns(showColumnsCheckbox.checked);
         localStorage.setItem("showColumns", showColumnsCheckbox.checked);
     });
 
-    // Функции для отображения реальных IP
-    function toggleRealIP(showRealIP) {
-        const ipCells = document.querySelectorAll(".real-ip-cell");
-        ipCells.forEach(cell => {
-            cell.textContent = showRealIP ? cell.getAttribute("data-real-ip") : cell.getAttribute("data-masked-ip");
-        });
-    }
-
-    // Функции для отображения столбцов скорости
     function toggleColumns(showColumns) {
         speedColumns.forEach(column => column.classList.toggle("hidden", !showColumns));
     }
 
-    // Функция для преобразования времени в локальное, используя data-атрибуты
     function convertToUserTimezone() {
         const timeCells = document.querySelectorAll('.connection-time');
 
@@ -61,7 +33,5 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
-    // Вызов функции для преобразования времени
     convertToUserTimezone();
-
 });
